03c9ba1ae755c0224981660ee603a9ce708de93f
Nafis Bin Azad <nafis123456789@gmail.com>
2023-07-22 23:47:40 -0700

    Displaying Platform Icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,26 +1,42 @@
+import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-interface Game {
+export interface Platform {
+  id: string;
+  name: string;
+  slug: string;
+}
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
-interface FetchGameResponse {
+
+interface FetchGamesResponse {
   count: number;
   results: Game[];
 }
+
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const controller = new AbortController();
+
     apiClient
-      .get<FetchGameResponse>("/games", { signal: controller.signal })
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
     return () => controller.abort();
   }, []);
+
   return { games, error };
 };
 
